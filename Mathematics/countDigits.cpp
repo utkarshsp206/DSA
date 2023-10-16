27e0b01f093c7703fe0965d829b276620ac732ca
Utkarsh-Singh <utkarshsp206@gmail.com>
2023-10-16 18:02:02 +0530

    Mathematics --> countDigits

---
+++ Mathematics/countDigits.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int countDigits(int num){
+    int res=0;
+    while(num>0){
+        num = num / 10;
+        res++;
+    }
+    return res;
+} 
+
+int main() {
+    int num;
+    cout<<"Enter any number: ";
+    cin>>num;
+    cout<<"Number of Digits in "<<num<<" is "<<countDigits(num);
+    return 0;
+
+}
