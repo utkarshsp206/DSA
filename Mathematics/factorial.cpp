d041688cc147bcd891d14d43fba59ae34ebe1940
Utkarsh-Singh <utkarshsp206@gmail.com>
2023-10-17 16:05:24 +0530

    Factorial

---
+++ Mathematics/factorial.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n){
+    if(n==0)
+    return 1;
+    int fact = 1;
+    fact = factorial(n-1)*n;
+    return fact;
+}
+ 
+int main() {
+int n;
+cout<<"Enter any number: ";
+cin>>n;
+cout<<"Factorial of "<<n<<" is "<<factorial(n);
+
+return 0;
+}
+
+//TC: O(n)
