53c52ed06a19b4491424ec106c323b2273f7a80a
Utkarsh-Singh <utkarshsp206@gmail.com>
2024-02-19 12:02:30 +0530

    list

---
+++ c++ STL/list.cpp
@@ -0,0 +1,35 @@
+/*
+A list in STL is a contiguous container that allows the inserting and erasing of elements in constant time and iterating in both directions.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int countDigits(int n){
+	// Write your code here.	
+	list<int> s;
+	int temp = n;
+	while(temp !=0){
+		int rem = temp%10;
+		temp = temp/10;
+		s.push_back(rem);
+	}
+    int count = 0;
+    for(auto t: s){
+        if(n%t==0){
+            count++;
+        }
+    }
+
+    return count;
+
+}
+
+int main() {
+int n;
+cout<<"Enter the value of n: ";
+cin>>n;
+cout<<countDigits(n);
+
+return 0;
+}
