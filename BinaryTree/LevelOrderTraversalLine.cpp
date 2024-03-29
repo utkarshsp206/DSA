d84bd35d03d1afafab9e226fc0f589194b8ae2a7
Utkarsh-Singh <utkarshsp206@gmail.com>
2024-03-29 13:34:41 +0530

    LevelOrder

--- BinaryTree/LevelOrderTraversalLine.cpp
+++ BinaryTree/LevelOrderTraversalLine.cpp
@@ -4,23 +4,30 @@ using namespace std;
 class Node{
     public:
     int data;
-    Node *left;    
-    Node *right;    
+    Node *left;
+    Node *right;
 
     Node(int val){
         data = val;
         left = NULL;
         right = NULL;
     }
+
 };
 
-void printLevel(Node *root){
+void printLevelLine(Node *root){
     if(root==NULL) return;
     queue<Node *> q;
     q.push(root);
-    while(q.empty()==false){
+    q.push(NULL);
+    while(q.size()>1){
         Node *curr = q.front();
         q.pop();
+        if(curr==NULL){
+            cout<<"\n";
+            q.push(NULL);
+            continue;
+        }
         cout<<curr->data<<" ";
         if(curr->left!=NULL){
             q.push(curr->left);
@@ -30,19 +37,18 @@ void printLevel(Node *root){
         }
     }
 }
- 
+
 int main() {
 
 Node *root = new Node(1);
 root->left = new Node(2);
 root->right = new Node(3);
-root->right->right = new Node(4);
-root->right->left = new Node(5);
-root->left->left = new Node(6);
-root->left->right = new Node(7);
+root->left->left = new Node(4);
+root->left->right = new Node(5);
+root->right->left = new Node(6);
+root->right->right = new Node(7);
 
-cout<<"Level order traversal is : ";
-printLevel(root);
+printLevelLine(root);
 
 return 0;
 }
