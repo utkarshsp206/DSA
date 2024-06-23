7f6c1d0851b8127fb8fc2f4c3731970ae6a9e30a
Utkarsh-Singh <utkarshsp206@gmail.com>
2024-06-23 15:57:58 +0530

    Array to LL and DLL

---
+++ LinkedList/ArraytoDLL.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public: 
+    int data;
+    Node *prev;
+    Node *next;
+
+    Node(int val){
+        data=val;
+        prev=NULL;
+        next=NULL;
+    }
+};
+
+void Display(Node *head){
+    Node *temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+Node *ArraytoDLL(vector<int> arr){
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+
+    for(int i=1;i<arr.size();i++){
+        Node *temp = new Node(arr[i]);
+        mover->next = temp;
+        temp->prev=mover;
+        mover=temp;
+    }
+return head;
+}
+ 
+int main() {
+vector<int> arr = {1,2,3,4,5};
+Node *head = ArraytoDLL(arr);
+Display(head);
+
+return 0;
+}
